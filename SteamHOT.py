168b2912b526c9eaca53b6bec959401c3c62b2c8
Pengcheng Pan <icue126@gmail.com>
2020-05-11 13:05:10 -0700

    Refactor and comments.

--- SteamHOT.py
+++ SteamHOT.py
@@ -3,6 +3,7 @@ import sys
 import json
 import requests
 import operator
+from collections import defaultdict
 from tqdm import tqdm
 from bs4 import BeautifulSoup
 from plotly.graph_objs import *
@@ -23,27 +24,26 @@ maxThreshold = 10000.0
 tagLimit = 100
 achievRestrict = False
 
-playerId = raw_input("Your steam id: ")
-minThresholdInput = raw_input("Minimum hours (default 3): ")
-maxThresholdInput = raw_input("Maximum hours (default 10000): ")
-tagLimitInput = raw_input("Tag limit (default none): ")
-achievRestrictInput = raw_input("Ignore games with no achievement unlocked? (Default NO)\nNote: This will not affect games that have no achievements.\n      This will take way longer time. (Y/N): ")
+# Gets and interprets input
+playerId = input("Your steam id: ")
+minThresholdInput = input("Minimum hours (default 3): ")
+maxThresholdInput = input("Maximum hours (default 10000): ")
+tagLimitInput = input("Tag limit (default none): ")
+achievRestrictInput = input("Ignore games with no achievement unlocked? (Default NO)\nNote: This will not affect games that have no achievements.\n      This will take a lot longer time. (Y/N): ")
 
-print("\nGenerating game list...")
 if(isNumber(playerId)):
     url = "http://steamcommunity.com/profiles/" + playerId 
 else:
     url = "http://steamcommunity.com/id/" + playerId
-
 if isNumber(minThresholdInput):
     minThreshold = float(minThresholdInput)
 if isNumber(maxThresholdInput):
     maxThreshold = float(maxThresholdInput)
 if isNumber(tagLimitInput):
     tagLimit = float(tagLimitInput)
-if achievRestrictInput == 'Y':
-    achievRestrict = True
+achievRestrict = achievRestrictInput == 'Y'
 
+print("\nGenerating game list...")
 
 cookies = { 'birthtime': '283993201', 'mature_content': '1' }
 page = requests.get(url + "/games/?tab=all" , cookies=cookies).text
@@ -51,14 +51,16 @@ want = json.loads(re.search(r"var rgGames = (.*);", page).group(1))
 
 gameHourList = {}
 nameDict = {}
-whiteList = [242050]
+whiteList = []
 
+# Shows progress bar #1
 t1 = tqdm(want)
 for eachJson in t1:
     hasAchiev = True
     appID = eachJson['appid']
-    gameName = eachJson['name'].encode('ascii', 'ignore')
+    gameName = eachJson['name']
     if achievRestrict:
+        # Excludes games with no achievements unlocked
         if eachJson['availStatLinks']['achievements']:
             achievUrl = url + "/stats/" + str(appID) + "/?tab=achievements"
             achievPage = requests.get(achievUrl, cookies=cookies).content
@@ -69,49 +71,43 @@ for eachJson in t1:
                 if summaryText.startswith('0') and not summaryText.startswith('0 of 0') and not appID in whiteList:
                     print("\nThis game will be ignored because there is no achievement unlocked: " + gameName)
                     continue
-    if 'hours_forever' in eachJson and float(eachJson['hours_forever'].replace(",", "")) > maxThreshold:
-        print("\nThis game will be ignored because its number of hours exceed maximum threshold: " + gameName)
-        continue
-    if 'hours_forever' in eachJson and float(eachJson['hours_forever'].replace(",", "")) < minThreshold:
-        print("\n" + gameName + " and other games with low hours will be ignored.")
-        break
-    gameHourList[appID] = float(eachJson['hours_forever'].replace(",", "")) if 'hours_forever' in eachJson else 0.0
+    # Gets number of hours
+    hours = 0
+    if 'hours_forever' in eachJson:
+        hours = float(eachJson['hours_forever'].replace(",", ""))
+        # Excludes games with number of hours > maximum threshold
+        if hours > maxThreshold:
+            print("\nThis game will be ignored because its number of hours exceed maximum threshold: " + gameName)
+            continue
+        # Excludes games with number of hours < minimum threshold. The list is already sorted, so we direclty break here.
+        if hours < minThreshold:
+            print("\n" + gameName + " and other games with low number of hours will be ignored.")
+            break
+    gameHourList[appID] = hours
     nameDict[appID] = gameName 
 
-sumList = {}
+print("There are " + str(len(gameHourList)) + " games to analyze in total.\nStarting the analysis...")
 
-print("There are " + str(len(gameHourList)) + " games to analyze in total.")
-
-print("\nStarting the analysis...")
-t = tqdm(gameHourList.items())
-for appid, hour in t:
+sumDict = defaultdict(float)
+# Shows progress bar #2
+t2 = tqdm(gameHourList.items())
+# Generates the tag->hours dict
+for appid, hour in t2:
     gameUrl = "http://store.steampowered.com/app/" + str(appid)
     gamePage = requests.get(gameUrl, cookies=cookies).content
     soup = BeautifulSoup(gamePage, 'html.parser')
-    t.set_description('Analyzing game %s' % nameDict[appid])
+    t2.set_description('Analyzing game {}'.format(nameDict[appid]))
     tags = soup.find_all("a", class_="app_tag")
     count = 1
     for tag in tags:
         tag = tag.get_text().strip()
-        if tag in sumList:
-            sumList[tag] += hour
-        else:
-            sumList[tag] = hour
+        sumDict[tag] += hour
         count+=1
         if(count>tagLimit):
             break
 
-# sortedSumList = sorted(sumList.items(), key=operator.itemgetter(1), reverse=True)
-sortedSumList = sumList.items()
-
-keys = []
-values = []
-for element in sortedSumList:
-    keys.append(element[0])
-    values.append(element[1])
-
-
-trace = Pie(labels=keys, values=values,
+# Draws the pie
+trace = Pie(labels=list(sumDict.keys()), values=list(sumDict.values()),
                hoverinfo='label+percent', textinfo='value', 
                textfont=dict(size=20), hole=0.4)
 data=[trace]
@@ -129,5 +125,5 @@ layout = {
             }
         ]
     }
-fig = dict( data=data, layout=layout )
+fig = dict(data=data, layout=layout)
 plot(fig, filename=playerId+".html")
